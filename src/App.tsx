e416c2e97da0b3793d67fc7372b3ef5ebbc6a477
Ernneth Toh <ernneth@gmail.com>
2025-08-18 12:05:44 +0800

    import colours, preview colours and refactor typography preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import './assets/fonts/typography.css';
+import "./assets/fonts/typography.css";
+import "./assets/colors/colors.css";
+import "./assets/colors/gradients.css";
+import ColorsPreview from "./assets/colors/colors-preview";
+import TypographyPreview from "./assets/fonts/typography-preview";
 
 function App() {
   return (
@@ -13,28 +17,13 @@ function App() {
           </p>
         </header>
         {/* Cards and Buttons removed as requested */}
+
         {/* Typography Preview */}
-        <section className="border-2 p-4">
-          <h2 className="mb-4 text-2xl font-bold underline">Typography Preview</h2>
-          <div>
-            <div className="heading-1 mb-2">Heading 1</div>
-            <div className="heading-2 mb-2">Heading 2</div>
-            <div className="heading-3 mb-2">Heading 3</div>
-            <div className="heading-4 mb-2">Heading 4</div>
-            <div className="heading-5 mb-2">Heading 5</div>
-            <div className="heading-6 mb-2">Heading 6</div>
-            <div className="subtitle-1 mb-2">Subtitle 1</div>
-            <div className="subtitle-2 mb-2">Subtitle 2</div>
-            <div className="subtitle-3 mb-2">Subtitle 3</div>
-            <div className="body-1 mb-2">Body 1</div>
-            <div className="body-2 mb-2">Body 2</div>
-            <div className="button mb-2">Button</div>
-            <div className="caption mb-2">Caption</div>
-            <div className="overline mb-2">Overline</div>
-            <div className="monospace mb-2">monospace</div>
-            <div className="sublabel mb-2">Sublabel</div>
-          </div>
-        </section>
+        <TypographyPreview />
+
+        {/* Colors Preview */}
+        <ColorsPreview />
+
       </div>
     </div>
   );
