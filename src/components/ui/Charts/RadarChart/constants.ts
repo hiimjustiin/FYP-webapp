343dff74ba1d2dd9f4a55c72958fc43bdabca3e1
Ernneth Toh <etoh011@e.ntu.edu.sg>
2025-09-10 14:41:19 +0800

    initial implementation of radar chart

---
+++ src/components/ui/Charts/RadarChart/constants.ts
@@ -0,0 +1,11 @@
+export const ALL_DIMENSIONS = [
+    'Frame the problem with an integrative approach',
+    'Stakeholder consideration',
+    'Range of disciplinary perspectives',
+    'Disciplinary reasoning',
+    'Credibility of disciplinary knowledge',
+    'Number of disciplinary integration',
+    'Depth of disciplinary integration',
+    'Social (society) impact',
+    'Limitations'
+];
