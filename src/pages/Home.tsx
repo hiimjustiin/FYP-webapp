f4000e9251ce6260ad3854dfa39a531160f72e8e
Derek <59902137+jdanas@users.noreply.github.com>
2025-10-01 16:23:14 +0800

    fix(Home): display user's name in greeting on the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,24 @@
+import { useAuth } from "../contexts/AuthContext";
 
 const Home = () => {
-    return (
-        <div className="p-4 sm:p-6 lg:p-8">
-            <div className="mx-auto max-w-7xl">
-                <div className="space-y-6">
-                    <div className="dashboard-card px-4 py-2">
-                        <h5 className="heading-5">Hi User!</h5>
-                        <p className="subtitle-2 text-grey-80">Let’s begin a new project with ILA!</p>
-                    </div>
-                </div>
-            </div>
+  const { user } = useAuth();
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="mx-auto max-w-7xl">
+        <div className="space-y-6">
+          <div className="dashboard-card px-4 py-2">
+            <h5 className="heading-5">
+              Hi{user?.display_name ? `, ${user.display_name}` : ""}!
+            </h5>
+            <p className="subtitle-2 text-grey-80">
+              Let's begin a new project with ILA!
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
